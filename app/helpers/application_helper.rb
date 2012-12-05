09be6ef75b606b977f330224a7794d8d45af7c89
Nuttapon Achachotipong <nuttapon@code-app.com>
2012-12-05 18:01:18 +0700

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,17 @@
+module ApplicationHelper
+  def broadcast(channel, &block)
+    message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
+    uri = URI.parse("http://codeapp-fayeserver.herokuapp.com/faye")
+    Net::HTTP.post_form(uri, :message => message.to_json)
+  end
+
+  def title(page_title, show_title = true)
+    content_for(:title) { h(page_title.to_s) }
+    @show_title = show_title
+  end
+
+  def show_title?
+    @show_title
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeappChat::Application.config.secret_token = '866cc4c770835315d97727b7c9203494ba9014b1c099cdc32125edc18092aff01e87f4863a8901066840c523fe1bec4c9dc114b3cee08a19fff24104b081b4d2'
